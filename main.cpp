ff5a49d53e18128a91988efed20190eb8bb3426f
GHC <goncalo.horacarvalho@gmail.com>
2022-02-28 09:40:53 +0100

    command line takes in matrix size

--- main.cpp
+++ main.cpp
@@ -3,13 +3,23 @@
 
 using namespace std;
 
-int main()
+int main(int argc, char *argv[])
 {
-    World world;
 
-    int doomsday; 
+    // size_t rows;
+    // size_t columns;
+    // cout << "N. Rows: ";
+    // cin >> rows;
+    // cout << "N. Columns: ";
+    // cin >> columns;
+
+    World world(atoi(argv[1]), atoi(argv[2]));
+
+
+
+    int doomsday;
     cout << "Iterations: ";
-    cin >> doomsday; // Get user input from the keyboard
+    cin >> doomsday;
     
     world.run(doomsday);
 }
